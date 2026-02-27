ad9b4e91e8abf2147c86e1f73d945c55a93c28ff
Matej Janežič <janezic.mj@gmail.com>
2026-02-27 03:40:31 -0500

    feat: bump ahab to v0.4.1

--- packages/ahab.nix
+++ packages/ahab.nix
@@ -1,7 +1,7 @@
 { pkgs, ... }:
 
 let
-  version = "v0.3.2";
+  version = "v0.4.1";
 in
 pkgs.rustPlatform.buildRustPackage {
   pname = "ahab";
@@ -12,10 +12,10 @@ pkgs.rustPlatform.buildRustPackage {
     owner = "janezicmatej";
     repo = "ahab";
     rev = version;
-    sha256 = "sha256-bL8LPpD5k97XPYftXhPr7V/LNOB71pcBlsfBjJUIeG8";
+    sha256 = "sha256-Y8UqZOskSlt8GrYem97yKXNbGkd6Ab7WRynKEA9w16E=";
   };
 
-  cargoHash = "sha256-f8omNtjLF5gMJGgxdzWifStcs8d4fu++EegR2auObXE";
+  cargoHash = "sha256-T5r+Og3+mHMsqCFGi+QzHdN2MgvPxzA/R+xu38I+lcg=";
 
   buildType = "debug";
 
