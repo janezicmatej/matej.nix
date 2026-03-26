76e74f4939ce564006434f966399ea2b9ba3f4b4
Matej Janežič <janezic.mj@gmail.com>
2026-03-26 22:35:23 +0100

    feat: switch from flake-utils to flake-parts

--- flake.nix
+++ flake.nix
@@ -6,10 +6,6 @@
     nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
     nixpkgs-master.url = "github:nixos/nixpkgs/master";
 
-    # dotfiles = {
-    #   url = "git+https://git.janezic.dev/janezicmatej/.dotfiles.git";
-    #   flake = false;
-    # };
     nvim = {
       url = "git+https://git.janezic.dev/janezicmatej/nvim.git";
       flake = false;
@@ -17,7 +13,7 @@
 
     nixos-hardware.url = "github:NixOS/nixos-hardware/master";
 
-    flake-utils.url = "github:numtide/flake-utils";
+    flake-parts.url = "github:hercules-ci/flake-parts";
 
     home-manager = {
       url = "github:nix-community/home-manager/release-25.11";
@@ -40,118 +36,115 @@
     };
 
     neovim-nightly-overlay.url = "github:nix-community/neovim-nightly-overlay";
-
   };
 
   outputs =
-    inputs@{
-      nixpkgs,
-      flake-utils,
-      ...
-    }:
+    inputs@{ flake-parts, nixpkgs, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } {
+      systems = [ "x86_64-linux" ];
 
-    let
-      my-lib = import ./lib { inherit (nixpkgs) lib; };
+      perSystem =
+        { system, pkgs, ... }:
+        let
+          my-lib = import ./lib { inherit (nixpkgs) lib; };
+        in
+        {
+          packages =
+            import ./packages
+              {
+                inherit my-lib;
+                inherit (nixpkgs) lib;
+              }
+              {
+                inherit pkgs;
+                pkgs-unstable = inputs.nixpkgs-unstable.legacyPackages.${system};
+                pkgs-master = inputs.nixpkgs-master.legacyPackages.${system};
+              };
 
-      overlays = [
-        (
-          _: prev:
-          let
-            pkgs-unstable = import inputs.nixpkgs-unstable {
-              inherit (prev.stdenv.hostPlatform) system;
-              inherit (prev) config;
-            };
-            pkgs-master = import inputs.nixpkgs-master {
-              inherit (prev.stdenv.hostPlatform) system;
-              inherit (prev) config;
-            };
-          in
-          {
-            inherit (pkgs-master) claude-code;
-            # TODO:(@janezicmatej) 2026-03-09 error with stable for telegram-desktop
-            inherit (pkgs-unstable) telegram-desktop;
-          }
-        )
-      ];
+          formatter = pkgs.nixfmt-tree;
 
-      mkHost = my-lib.mkHost {
-        inherit
-          nixpkgs
-          overlays
-          inputs
-          ;
-      };
-
-    in
-
-    {
-      lib = my-lib;
-
-      nixosConfigurations = {
-        fw16 = mkHost "fw16" {
-          system = "x86_64-linux";
-          user = "matej";
-        };
-        tower = mkHost "tower" {
-          system = "x86_64-linux";
-          user = "matej";
+          devShells.default = pkgs.mkShell {
+            packages = [
+              pkgs.pre-commit
+              pkgs.statix
+              pkgs.shellcheck
+              pkgs.shfmt
+              pkgs.qemu
+            ];
+          };
         };
 
-        # nixos-rebuild build-image --image-variant install-iso --flake .#iso
-        iso = mkHost "iso" {
-          system = "x86_64-linux";
-        };
+      flake =
+        let
+          my-lib = import ./lib { inherit (nixpkgs) lib; };
 
-        ephvm = mkHost "ephvm" {
-          system = "x86_64-linux";
-          user = "matej";
-        };
-      };
-
-      nixosModules = import ./modules/nixos {
-        inherit my-lib;
-        inherit (nixpkgs) lib;
-      } { };
-
-      homeManagerModules = import ./modules/home-manager {
-        inherit my-lib;
-        inherit (nixpkgs) lib;
-      } { };
-
-      nixosProfiles = import ./profiles {
-        inherit my-lib;
-        inherit (nixpkgs) lib;
-      } { };
-    }
-    // flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = nixpkgs.legacyPackages.${system};
-      in
-      {
-        packages =
-          import ./packages
-            {
-              inherit my-lib;
-              inherit (nixpkgs) lib;
-            }
-            {
-              pkgs = nixpkgs.legacyPackages.${system};
-              pkgs-unstable = inputs.nixpkgs-unstable.legacyPackages.${system};
-              pkgs-master = inputs.nixpkgs-master.legacyPackages.${system};
-            };
-
-        formatter = pkgs.nixfmt-tree;
-
-        devShells.default = pkgs.mkShell {
-          packages = [
-            pkgs.pre-commit
-            pkgs.statix
-            pkgs.shellcheck
-            pkgs.shfmt
-            pkgs.qemu
+          overlays = [
+            (
+              _: prev:
+              let
+                pkgs-unstable = import inputs.nixpkgs-unstable {
+                  inherit (prev.stdenv.hostPlatform) system;
+                  inherit (prev) config;
+                };
+                pkgs-master = import inputs.nixpkgs-master {
+                  inherit (prev.stdenv.hostPlatform) system;
+                  inherit (prev) config;
+                };
+              in
+              {
+                inherit (pkgs-master) claude-code;
+                # TODO:(@janezicmatej) 2026-03-09 error with stable for telegram-desktop
+                inherit (pkgs-unstable) telegram-desktop;
+              }
+            )
           ];
+
+          mkHost = my-lib.mkHost {
+            inherit
+              nixpkgs
+              overlays
+              inputs
+              ;
+          };
+        in
+        {
+          lib = my-lib;
+
+          nixosConfigurations = {
+            fw16 = mkHost "fw16" {
+              system = "x86_64-linux";
+              user = "matej";
+            };
+            tower = mkHost "tower" {
+              system = "x86_64-linux";
+              user = "matej";
+            };
+
+            # nixos-rebuild build-image --image-variant install-iso --flake .#iso
+            iso = mkHost "iso" {
+              system = "x86_64-linux";
+            };
+
+            ephvm = mkHost "ephvm" {
+              system = "x86_64-linux";
+              user = "matej";
+            };
+          };
+
+          nixosModules = import ./modules/nixos {
+            inherit my-lib;
+            inherit (nixpkgs) lib;
+          } { };
+
+          homeManagerModules = import ./modules/home-manager {
+            inherit my-lib;
+            inherit (nixpkgs) lib;
+          } { };
+
+          nixosProfiles = import ./profiles {
+            inherit my-lib;
+            inherit (nixpkgs) lib;
+          } { };
         };
-      }
-    );
+    };
 }
