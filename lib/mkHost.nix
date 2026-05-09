fae6b251375ae49320a96ac8177fba587d554d17
Matej Janežič <janezic.mj@gmail.com>
2026-05-09 23:22:35 +0200

    temp: dirty-frag (CVE-2026-43284, CVE-2026-43500)

--- lib/mkHost.nix
+++ lib/mkHost.nix
@@ -87,6 +87,11 @@ nixpkgs.lib.nixosSystem {
     { nixpkgs.config.allowUnfree = true; }
     { networking.hostName = name; }
 
+    # TEMP:(@janezicmatej) temporary mitigation for dirty frag
+    # blocks esp4/esp6 (CVE-2026-43284) and rxrpc (CVE-2026-43500)
+    # remove once nixpkgs ships a kernel with f4c50a4034e6 and the rxrpc fix
+    { boot.blacklistedKernelModules = [ "esp4" "esp6" "rxrpc" ]; }
+
     featureEnableModule
     hostConfig
   ]
