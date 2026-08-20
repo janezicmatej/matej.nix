a0a7ef17d21ec5b24b00e170100c87d38e812450
Matej Janežič <janezic.mj@gmail.com>
2026-08-20 15:50:34 +0200

    temp: pin rocketchat-desktop to stable

--- flake/overlays.nix
+++ flake/overlays.nix
@@ -2,6 +2,10 @@
 
 {
   flake.overlays.default = final: _prev: {
-    inherit (inputs.nixpkgs-stable.legacyPackages.${final.stdenv.hostPlatform.system}) mcp-nixos;
+    inherit (inputs.nixpkgs-stable.legacyPackages.${final.stdenv.hostPlatform.system})
+      mcp-nixos
+      # v4.16.0 not in supported versions yet
+      rocketchat-desktop
+      ;
   };
 }
